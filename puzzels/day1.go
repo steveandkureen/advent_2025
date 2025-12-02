fddfab60154397aa5f7a05ee743d9fffce40b4a0
Stephen Schwarz <steve@steveandkureen.com>
2025-12-01 18:08:27 -0700

    initial commit of day one

---
+++ puzzels/day1.go
@@ -0,0 +1,91 @@
+package puzzels
+
+import (
+	"fmt"
+	"steveandkureen/advent_2025/internal"
+	"strconv"
+	"strings"
+)
+
+func FindCombonation(filename string, isHard bool) (string, error) {
+	data, err := internal.ReadFile(filename)
+	if err != nil {
+		fmt.Printf("error reading file: %s\n", err)
+	}
+	fmt.Printf("Input: %s\n", string(data))
+	if err != nil {
+		return "", err
+	}
+	currentPos := 50
+	steps := strings.Split(data, "\n")
+	result := 0
+	for _, line := range steps {
+		if len(line) == 0 {
+			continue
+		}
+		direction := line[0]
+		num, err := strconv.Atoi(line[1:])
+		if err != nil {
+			fmt.Printf("Bad data found %s\n", line)
+		}
+
+		if isHard {
+			result, currentPos = doHardCombo(direction, num, currentPos, result)
+		} else {
+			result, currentPos = doEasyCombo(direction, num, currentPos, result)
+		}
+	}
+	fmt.Printf("Answer: %d\n", result)
+	return fmt.Sprintf("%d", result), nil
+}
+
+func doEasyCombo(direction byte, num int, currentPos int, result int) (int, int) {
+	if direction == 'L' {
+		currentPos -= num
+	} else {
+		currentPos += num
+	}
+
+	for currentPos < 0 {
+		currentPos += 100
+	}
+	for currentPos > 99 {
+		currentPos -= 100
+	}
+
+	if currentPos == 0 {
+		result++
+	}
+	return result, currentPos
+}
+
+func doHardCombo(direction byte, num int, currentPos int, result int) (int, int) {
+	startPos := currentPos
+	result += num / 100
+
+	num = num % 100
+
+	if direction == 'L' {
+		currentPos -= num
+	} else {
+		currentPos += num
+	}
+
+	if currentPos < 0 {
+		currentPos += 100
+		if currentPos != 0 && startPos != 0 {
+			result++
+		}
+	} else if currentPos > 99 {
+		currentPos -= 100
+		if currentPos != 0 {
+			result++
+		}
+	}
+
+	if currentPos == 0 {
+		result++
+	}
+
+	return result, currentPos
+}
